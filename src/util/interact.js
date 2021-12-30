dff525e15b906738f117f3a7278e67ff49dfa17e
Hideto Yasunori <hideto0118yasunori@gmail.com>
2021-12-29 16:56:55 -0800

    Add basic consts on interact.js

--- src/util/interact.js
+++ src/util/interact.js
@@ -1,4 +1,14 @@
-//export const helloWorldContract;
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchhemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+const contractABI = require("../contract-abi.json");
+const contractAddress = "0x7d8276f06b6bc648cCAAb798b233Cdf0987cE224";
+
+export const helloWorldContract = new web3.eth.Contract(
+  contractABI,
+  contractAddress
+);
 
 export const loadCurrentMessage = async () => { 
   
